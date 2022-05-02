ef50d209bf85da93d9cd93fdf71e0f95b7fb10d4
Tuscanny Polk <tuscannypolk@gmail.com>
2022-05-02 15:26:12 -0400

    WORKING tweaked button css and cleaned code

--- src/App.js
+++ src/App.js
@@ -5,16 +5,12 @@ import useGreetingsLogic from "./Components/useGreetingsLogic";
 import { useState } from "react";
 // import GreetingsHandler from "./Components/GreetingsHandler";
 function App() {
-  const {responseCallbackHandler,response,setResponse}= useGreetingsLogic();
-  
-  // state for responses
-  // const [response, setResponse] = useState("");
+  const { responseCallbackHandler, response, setResponse } =
+    useGreetingsLogic();
 
   // this will be the main function that calls other functions.
-  
-  const Fn1 = () => {
-    // let userInput = document.getElementById("mainInputBox").value;
 
+  const Fn1 = () => {
     // thinking animation
 
     function thinking() {
@@ -29,48 +25,8 @@ function App() {
       }, 1600);
     }
     thinking();
-responseCallbackHandler();
-  
-    // <GreetingsHandler userInput={userInput} setResponse={setResponse}/>
-    // thinking animation END
-    // timer for after thinking animation
-    // want to take this entire function and put it into a seperate component
-    // setTimeout(function getGreetings() {
-    //   // greeting object
-
-    //   // use callback should be here
-    //   // use constome hook
-    //   const greetings = {
-    //     greets: ["hello", "hi", "hey", "good morning", "goodbye", "yo"],
-    //     responses: ["hello", "hello there", "hey there", "hi", "whats up?"],
-    //     getGreetResponse: function () {
-    //       // if user does not type anything
-    //       if (userInput === "" || userInput === " ") {
-    //         setResponse("you didnt say anything");
-    //       } // if user does not type anything END
-
-    //       // if statements for GREETINGS Object
-    //       // this loops over generic greetings and gives generic responses
-    //       for (let i = 0; i < greetings.greets.length; i++) {
-    //         if (userInput.includes(greetings.greets[i])) {
-    //           setResponse(
-    //             greetings.responses[
-    //               Math.floor(Math.random() * greetings.responses.length)
-    //             ]
-    //           );
-    //         }
-    //       }
-    //       // these are for more specific greetings
-    //       if (userInput.includes(greetings.greets[3])) {
-    //         setResponse("Morning :)");
-    //       }
-    //       if (userInput.includes(greetings.greets[4])) {
-    //         setResponse("Goodbye. See you later.");
-    //       }
-    //     }, // getGreetResponse method END
-    //   }; // greeting object END
-    //   greetings.getGreetResponse();
-    // }, 3000);
+    // end of thinking animation.
+    responseCallbackHandler();
   }; // end of fn1
 
   // this is rendering the components
@@ -82,17 +38,5 @@ responseCallbackHandler();
     </div>
   );
 }
-/**
- * app
- *  response label 
- *  user form
- *  chatbot control
- *   
- * 
- * */
-
-
-
-
 
 export default App;
--- src/Components/useGreetingsLogic.js
+++ src/Components/useGreetingsLogic.js
@@ -1,49 +1,47 @@
-
-import {useState} from 'react';
+import { useState } from "react";
 
 const useGreetingsLogic = () => {
-    const [response, setResponse] = useState("");
-    function responseCallbackHandler(){
-  setTimeout(function getGreetings() {
-    // greeting object
-    let userInput = document.getElementById("mainInputBox").value;
-    // use callback should be here
-    // use custom hook
-    const greetings = {
-      greets: ["hello", "hi", "hey", "good morning", "goodbye", "yo"],
-      responses: ["hello", "hello there", "hey there", "hi", "whats up?"],
-      getGreetResponse: function () {
-        // if user does not type anything
-        if (userInput === "" || userInput === " ") {
-          setResponse("you didnt say anything");
-        } // if user does not type anything END
+  // This controls the state for the text being displayed
+  const [response, setResponse] = useState(" ");
+  // callback function to place function inside of fn1
+  function responseCallbackHandler() {
+    setTimeout(function getGreetings() {
+      // greeting object
+      let userInput = document.getElementById("mainInputBox").value;
 
-        // if statements for GREETINGS Object
-        // this loops over generic greetings and gives generic responses
-        for (let i = 0; i < greetings.greets.length; i++) {
-          if (userInput.includes(greetings.greets[i])) {
-            setResponse(
-              greetings.responses[
-                Math.floor(Math.random() * greetings.responses.length)
-              ]
-            );
-          };
-        };
-        // these are for more specific greetings
-        if (userInput.includes(greetings.greets[3])) {
-          setResponse("Morning :)");
-        }
-        if (userInput.includes(greetings.greets[4])) {
-          setResponse("Goodbye. See you later.");
-        }
-      }, // getGreetResponse method END
-    }; // greeting object END
-    greetings.getGreetResponse();
-  }, 3000);
-}
-  return {responseCallbackHandler,response,setResponse};  
- };
+      const greetings = {
+        greets: ["hello", "hi", "hey", "good morning", "goodbye", "yo"],
+        responses: ["hello", "hello there", "hey there", "hi", "whats up?"],
+        getGreetResponse: function () {
+          // if user does not type anything
+          if (userInput === "" || userInput === " ") {
+            setResponse("you didnt say anything");
+          } // if user does not type anything END
 
-
+          // if statements for GREETINGS Object
+          // this loops over generic greetings and gives generic responses
+          for (let i = 0; i < greetings.greets.length; i++) {
+            if (userInput.includes(greetings.greets[i])) {
+              setResponse(
+                greetings.responses[
+                  Math.floor(Math.random() * greetings.responses.length)
+                ]
+              );
+            }
+          }
+          // these are for more specific greetings
+          if (userInput.includes(greetings.greets[3])) {
+            setResponse("Morning :)");
+          }
+          if (userInput.includes(greetings.greets[4])) {
+            setResponse("Goodbye. See you later.");
+          }
+        }, // getGreetResponse method END
+      }; // greeting object END
+      greetings.getGreetResponse();
+    }, 3000);
+  }
+  return { responseCallbackHandler, response, setResponse };
+};
 
 export default useGreetingsLogic;
