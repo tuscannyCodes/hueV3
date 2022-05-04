769bbf39b6afc70ebcd9c7c66f8836b6cbd928b2
Tuscanny Polk <tuscannypolk@gmail.com>
2022-05-03 23:10:22 -0400

    WORKING kinda/ building time response

--- src/Components/useTimeResponseLogic.js
+++ src/Components/useTimeResponseLogic.js
@@ -1,17 +1,18 @@
+import { useState }from "react";
+
 const useTimeResponseLogic = (setResponse) => {
   // This controls the state for the text being displayed
   //   const [response, setResponse] = useState(".__.");
   // callback function to place function inside of fn1
 
-  // stuck here for now having issues declaring variable.
   
+const [ theCurrentTime, setTheCurrentTime] = useState("hello");
   function timeResponseCallbackHandler() {
     setTimeout(function getTimeResponse() {
       // let time = new Date(); // This is for the time.
       let userInput = document.getElementById("mainInputBox").value;
       //TIME RESPONSE
-      if (userInput.includes(" time")){
-        
+      if (userInput.includes(" time")) {
         currentTime();
         function currentTime() {
           let updatedTime = new Date().toLocaleString("en-US", {
@@ -20,18 +21,15 @@ const useTimeResponseLogic = (setResponse) => {
             //second: "numeric",
             hour12: true,
           });
-          setResponse("the time is " + updatedTime);
-          // *** this displays "the time is [object object]".
-          
-      
-        };
-       
-      };
-    },2600);
-    
-  };
-  return {  timeResponseCallbackHandler };
+          setTheCurrentTime(updatedTime);
+          setResponse(`${"The time is "}` + theCurrentTime);
+          // *** this displays "the time as initial state then after displays old time".
+        }
+      }
+    }, 2600);
+    console.log(theCurrentTime)
+  }
+  return { timeResponseCallbackHandler };
 };
-
 
 export default useTimeResponseLogic;
