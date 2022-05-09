94f6ac4fcbb3eb3d1a917db96779019810b71b4b
Tuscanny Polk <tuscannypolk@gmail.com>
2022-05-09 15:27:23 -0400

    WORKING time response is working and thanks response is working

--- src/Components/useFn2Logic.js
+++ src/Components/useFn2Logic.js
@@ -1,39 +1,34 @@
 import { useState } from "react";
 import useGreetingsLogic from "./useGreetingsLogic";
 import useTimeResponseLogic from "./useTimeResponseLogic";
+import useThanksLogic from "./useThanksLogic";
 const useFn2Logic = () => {
-  
- 
-  
   // This controls the state for the text being displayed
   let dynamicTime;
-  
+
   // STATE
   const [response, setResponse] = useState(".__.");
- 
   // STATE END
+  const {ThanksCallbackHandler} = useThanksLogic(setResponse);
   const { timeResponseCallbackHandler } = useTimeResponseLogic(setResponse);
-  const { responseCallbackHandler } = useGreetingsLogic(response, setResponse);
+  const { responseCallbackHandler } = useGreetingsLogic(setResponse);
   // callback function to place function inside of fn1
   function fn2CallbackHandler() {
     function fn2() {
       let userInput = document.getElementById("mainInputBox").value;
-      
-      if(userInput.includes(" time")) {
-        
-        console.log("timer")
-        dynamicTime = setInterval(timeResponseCallbackHandler,1000)
-        //*this set interval is not being cleared  
-     
-      }
-      else{
-        console.log(userInput)
-        clearInterval(dynamicTime)
-        
+
+      if (userInput.includes(" time")) {
+        console.log("timer");
+        dynamicTime = setInterval(timeResponseCallbackHandler, 50);
+        //*this set interval is not being cleared
+      } else {
+        console.log(userInput);
+        clearInterval(dynamicTime);
       }
       responseCallbackHandler();
+      ThanksCallbackHandler();
     }
-    
+
     fn2();
   }
   return { fn2CallbackHandler, response, setResponse };
--- src/Components/useTimeResponseLogic.js
+++ src/Components/useTimeResponseLogic.js
@@ -1,7 +1,5 @@
 // importing state from main logical component "useFn2Logic"
 const useTimeResponseLogic = (setResponse) => {
-
-  
   // callback function to place function inside of fn1
   function timeResponseCallbackHandler() {
     getTimeResponse();
@@ -9,28 +7,19 @@ const useTimeResponseLogic = (setResponse) => {
       // ***need to replace this with ref
       let userInput = document.getElementById("mainInputBox").value;
       //TIME RESPONSE
-if(userInput.includes( "time")){
-      
+      if (userInput.includes("time")) {
         currentTime();
-        // *** NEET TO CLEAR THIS INTERVAL SOMEWHERE
-
         function currentTime() {
-          // ***place above in fn2
-         
           let updatedTime = new Date().toLocaleString("en-US", {
             hour: "numeric",
             minute: "numeric",
-            second: "numeric",
+            // second: "numeric",
             hour12: true,
           });
-console.log("timeresponse")
-
+          console.log("timeresponse");
           setResponse("the time is " + updatedTime);
-
         }
-        }
-      
-      
+      }
     }
   }
 
