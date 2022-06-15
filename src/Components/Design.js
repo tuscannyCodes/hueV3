29bc1abf55f6465cfa2a292ff32e7793b320c262
Tuscanny Polk <tuscannypolk@gmail.com>
2022-06-15 14:39:55 -0400

    working on adding images to cards

--- src/Components/Design.js
+++ src/Components/Design.js
@@ -3,18 +3,19 @@ import React from "react";
 function Design() {
   return (
       <>
+      
+      
     <div className="container designContainer">
-      <div class="row justify-content-center">
+      <div className="row justify-content-center">
         <div
-          className="card  m-5 rounded-3 border border-dark "
+          className="card  m-5 rounded-3 border border-dark"
           style={{ width: "20rem" }}
         >
-          <img src="..." className="card-img-top" alt="..."></img>
+          <img src="public/images/smiley-face.png" className="card-img-top" alt="..."></img>
           <div className="card-body">
             <h5 className="card-title">Portfolio Website</h5>
             <p className="card-text">
-              A design for an animated portfolio site using React JS and Framer
-              Motion.
+              A landing page layout design created using Bootstrap.
             </p>
             <a href="#" className="btn btn-dark">
               visit
@@ -25,7 +26,7 @@ function Design() {
           className="card m-5 rounded-3 border border-dark"
           style={{ width: "20rem" }}
         >
-          <img src="..." className="card-img-top" alt="..."></img>
+          <img src="public/images/portfolio.jpg" className="card-img-top" alt="..."></img>
           <div className="card-body">
             <h5 className="card-title">Bootstrap Website</h5>
             <p className="card-text">
@@ -41,10 +42,10 @@ function Design() {
 
 
 
-// part 2
+
 
 <div className="container designContainer2">
-      <div class="row justify-content-center">
+      <div className="row justify-content-center">
         <div
           className="card  m-5 rounded-3 border border-dark "
           style={{ width: "20rem" }}
@@ -65,9 +66,9 @@ function Design() {
           className="card m-5 rounded-3 border border-dark"
           style={{ width: "20rem" }}
         >
-          <img src="..." className="card-img-top" alt="..."></img>
+          <img src="public/images/simon.jpg" className="card-img-top" alt="..."></img>
           <div className="card-body">
-            <h5 className="card-title">Bootstrap Website</h5>
+            <h5 className="card-title">Simon Game Website</h5>
             <p className="card-text">
               A landing page layout design created using Bootstrap.
             </p>
