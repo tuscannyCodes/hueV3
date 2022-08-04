038deac07f83995366d0b35adbde3a37e65101fb
TuscannyCodes <tuscannypolk@gmail.com>
2022-08-04 05:34:17 -0400

    minor updates

--- src/Components/MainButton.js
+++ src/Components/MainButton.js
@@ -1,30 +1,47 @@
 import React from "react";
 
-
-
-
 function MainButton({ fn1, setResponse }) {
-  
-  document.addEventListener("keydown", function(e) {
-    if (e.keyCode === 13) {
-       fn1()
-    }
-});
+  // GLOBAL EVENTS START
+  // *******************FUNNY FACE FUNCTION START**************************
+  /* This is a function that makes a
+ face when window is tripple clicked. 
+ I will be placing global window and 
+ document events in this section. */
+  let TOUCH_TIMEOUT_MILLISECONDS = 500;
+  let touch_count = 0;
+  document.addEventListener("touchend", function (evt) {
+    touch_count += 1;
 
-//   function handleKeyDown(e){
-//     if(e.keyCode === 13) { 
-//       fn1();
-//         console.log('Enter key pressed')
-//   }
-// }
-  // state for the hue responses
+    setTimeout(function () {
+      touch_count = 0;
+    }, TOUCH_TIMEOUT_MILLISECONDS);
+
+    if (touch_count === 3) {
+      setTimeout(() => {
+        setResponse("●﹏●");
+      }, 50);
+    }
+
+    setTimeout(() => {
+      setResponse("._____.");
+    }, 1500);
+  });
+  // *******************FUNNY FACE FUNCTION END**************************
+
+  //'ENTER' Button event
+
+  document.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+      fn1();
+    }
+  });
+  //'ENTER' Button event END
+  // GLOBAL EVENTS END
+  
   return (
     <div className="MainContainer">
       <div className="Button-container">
-        <button onClick={fn1} className="MainButton" >
-          
-         
-        </button>
+        <button onClick={fn1} className="MainButton"></button>
       </div>
     </div>
   );
